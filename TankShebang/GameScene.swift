6547abbf340cc6e8372a6c97f8a3ff3442718ef2
Aryan Agarwal <spilltokling@gmail.com>
2019-11-21 15:34:14 -0500

    Update GameScene.swift

--- TankShebang/GameScene.swift
+++ TankShebang/GameScene.swift
@@ -8,6 +8,37 @@
 
 import SpriteKit
 import GameplayKit
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
 
 class GameScene: SKScene {
     
@@ -23,7 +54,6 @@ class GameScene: SKScene {
     var p2LeftPressed = false
     var p2RightPressed = false
     
-    let tankMoveSpeed = CGFloat(3)
     let tankRotateSpeed = 0.1 //tank turning speed
     
     
@@ -61,31 +91,24 @@ class GameScene: SKScene {
         addChild(player2Right)
         addChild(player2Left)
 
+        
+//        run(SKAction.repeatForever(
+//            SKAction.run(moveTanksForward)
+//        ))
+        
     }
     
     // Moves tank forward in the direction it is facing
     func moveTanksForward() {
         // some basic trigonometry to calculate direction tanks are moving
-        let p1Direction = CGPoint(x:player1.position.x - sin(player1.zRotation) * tankMoveSpeed,y:player1.position.y + cos(player1.zRotation) * tankMoveSpeed)
-        let p2Direction = CGPoint(x:player2.position.x - sin(player2.zRotation) * tankMoveSpeed,y:player2.position.y + cos(player2.zRotation) * tankMoveSpeed)
+        let p1Direction = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
+        let p2Direction = CGPoint(x:player2.position.x - sin(player2.zRotation) * 3,y:player2.position.y + cos(player2.zRotation) * 3)
         
         let moveTank1 = SKAction.move(to: p1Direction, duration:0)
         let moveTank2 = SKAction.move(to: p2Direction, duration:0)
         
         player1.run(moveTank1)
         player2.run(moveTank2)
-    }
-    
-    func fireProjectile(player: SKSpriteNode) {
-        let projectile = SKSpriteNode(imageNamed: "defaultProjectile")
-        let direction = CGPoint(x:player.position.x - sin(player.zRotation) * 1000,y:player.position.y + cos(player.zRotation) * 1000)
-        projectile.position = player.position
-         
-        addChild(projectile)
-        
-        let shoot = SKAction.move(to: direction, duration: 2.0)
-        let shootDone = SKAction.removeFromParent()
-        projectile.run(SKAction.sequence([shoot, shootDone]))
     }
     
     
@@ -110,8 +133,7 @@ class GameScene: SKScene {
             }
             // player 1 shoot button pressed
             else if (player1Right.contains(location)){
-//                p1RightPressed = true
-                fireProjectile(player: player1)
+                p1RightPressed = true
             }
             
             // player 2 turn button pressed
@@ -120,8 +142,7 @@ class GameScene: SKScene {
             }
             // player 2 shoot button pressed
             else if (player2Right.contains(location)){
-//                p2RightPressed = true
-                fireProjectile(player: player2)
+                p2RightPressed = true
             }
         }
     }
@@ -142,20 +163,70 @@ class GameScene: SKScene {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        var player = 0;
         for touch in touches {
             let location = touch.location(in:self)
             // if player 1 turn button no longer pressed
             if (player1Left.contains(location)){
+                
                 p1LeftPressed = false
             }
             if (player2Left.contains(location)){
+                
                 p2LeftPressed = false
             }
             if (player1Right.contains(location)){
+                player = 1
+                p1LeftPressed = false
             }
             if (player2Right.contains(location)){
+                player = 2
+                p1LeftPressed = false
             }
         }
+        //guard let touch = touches.first else {
+         //   return
+        //}
+        //let touchLocation = touch.location(in: self)
+        
+        // 2 - Set up initial location of projectile
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        var direc = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
+        if(player == 1){
+           projectile.position = player1.position
+            
+             direc = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
+            
+        }
+        if(player == 2){
+            projectile.position = player2.position
+             direc = CGPoint(x:player2.position.x - sin(player2.zRotation) * 3,y:player2.position.y + cos(player2.zRotation) * 3)
+        }
+       
+        
+        
+        // 3 - Determine offset of location to projectile
+        let offset = projectile.position
+        
+        // 4 - Bail out if you are shooting down or backwards
+        if offset.x < 0 { return }
+        
+        // 5 - OK to add now - you've double checked position
+        addChild(projectile)
+        
+        // 6 - Get the direction of where to shoot
+        let direction = offset.normalized()
+        
+        // 7 - Make it shoot far enough to be guaranteed off screen
+        let shootAmount = direction * 1000
+        
+        // 8 - Add the shoot amount to the current position
+        let realDest = direc * 1000
+        
+        // 9 - Create the actions
+        let actionMove = SKAction.move(to: direc, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
