d86d7891ee6833985211e1577e3330470418adc4
gaybrogrammer <rbeit508@gmail.com>
2019-11-24 03:43:51 -0500

    Add files via upload

--- TankShebang/GameScene.swift
+++ TankShebang/GameScene.swift
@@ -1,52 +1,36 @@
-//
-//  GameScene.swift
-//  TankShebang
-//
-//  Created by Sean Cao on 11/5/19.
-//  Copyright © 2019 Sean Cao, Robert Beit, Aryan Aru Agarwal. All rights reserved.
-//
 
 import SpriteKit
 import GameplayKit
-func +(left: CGPoint, right: CGPoint) -> CGPoint {
-    return CGPoint(x: left.x + right.x, y: left.y + right.y)
-}
-
-func -(left: CGPoint, right: CGPoint) -> CGPoint {
-    return CGPoint(x: left.x - right.x, y: left.y - right.y)
-}
-
-func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
-    return CGPoint(x: point.x * scalar, y: point.y * scalar)
-}
-
-func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
-    return CGPoint(x: point.x / scalar, y: point.y / scalar)
-}
-
-#if !(arch(x86_64) || arch(arm64))
-func sqrt(a: CGFloat) -> CGFloat {
-    return CGFloat(sqrtf(Float(a)))
-}
-#endif
-
-extension CGPoint {
-    func length() -> CGFloat {
-        return sqrt(x*x + y*y)
-    }
-    
-    func normalized() -> CGPoint {
-        return self / length()
-    }
+struct PhysicsCategory {
+    static let none      : UInt32 = 0
+    static let all       : UInt32 = UInt32.max
+    static let player1   : UInt32 = 0b1
+    static let player2   : UInt32 = 0b1 // 1
+    static let projectile: UInt32 = 0b10      // 2
 }
 
 class GameScene: SKScene {
+    
+    let playableRect: CGRect
+    
+    override init(size: CGSize) {
+        let maxAspectRatio:CGFloat = 16.0/19.0
+        let playableHeight = size.width / maxAspectRatio
+        let playableMargin = (size.height-playableHeight)/2.0
+        playableRect = CGRect(x: 0, y:playableMargin, width: size.width, height:playableHeight)
+        super.init(size:size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     let player1 = SKSpriteNode(imageNamed: "tank") //player 1 tank
     let player1Right = SKSpriteNode(imageNamed: "red") //player 1 shoot button
     let player1Left = SKSpriteNode(imageNamed: "red") //player 1 turn button
     var p1LeftPressed = false
     var p1RightPressed = false
+    var projectile_owner = 0
     
     let player2 = SKSpriteNode(imageNamed: "tank") //player 2 tank
     let player2Right = SKSpriteNode(imageNamed: "red") //player 2 shoot button
@@ -54,10 +38,13 @@ class GameScene: SKScene {
     var p2LeftPressed = false
     var p2RightPressed = false
     
+    let tankMoveSpeed = CGFloat(3)
     let tankRotateSpeed = 0.1 //tank turning speed
     
     
+    
     override func didMove(to view: SKView) {
+        
         
         backgroundColor = SKColor.white
         
@@ -90,25 +77,101 @@ class GameScene: SKScene {
         addChild(player2)
         addChild(player2Right)
         addChild(player2Left)
-
         
-//        run(SKAction.repeatForever(
-//            SKAction.run(moveTanksForward)
-//        ))
+        debugDrawPlayableArea()
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        player1.physicsBody = SKPhysicsBody(rectangleOf: player1.size) // 1
+        player1.physicsBody?.isDynamic = true // 2
+        player1.physicsBody?.categoryBitMask = PhysicsCategory.player1 // 3
+        player1.physicsBody?.contactTestBitMask = PhysicsCategory.projectile // 4
+        player2.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
+        player2.physicsBody = SKPhysicsBody(rectangleOf: player1.size) // 1
+        player2.physicsBody?.isDynamic = true // 2
+        player2.physicsBody?.categoryBitMask = PhysicsCategory.player2 // 3
+        player2.physicsBody?.contactTestBitMask = PhysicsCategory.projectile // 4
+        player2.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
+        
+    }
+    
+    func boundsChecktanks(){
+        let bottomLeft = CGPoint(x:0, y:playableRect.minY)
+        let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+        
+        if player1.position.x <= bottomLeft.x {
+            player1.position.x = bottomLeft.x
+        }
+        if player1.position.x >= topRight.x {
+            player1.position.x = topRight.x
+        }
+        if player1.position.y <= bottomLeft.y {
+            player1.position.y = bottomLeft.y
+        }
+        if player1.position.y >= topRight.y {
+            player1.position.y = topRight.y
+        }
+        
+        if player2.position.x <= bottomLeft.x {
+            player2.position.x = bottomLeft.x
+        }
+        if player2.position.x >= topRight.x {
+            player2.position.x = topRight.x
+        }
+        if player2.position.y <= bottomLeft.y {
+            player2.position.y = bottomLeft.y
+        }
+        if player2.position.y >= topRight.y {
+            player2.position.y = topRight.y
+        }
         
     }
     
     // Moves tank forward in the direction it is facing
     func moveTanksForward() {
         // some basic trigonometry to calculate direction tanks are moving
-        let p1Direction = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
-        let p2Direction = CGPoint(x:player2.position.x - sin(player2.zRotation) * 3,y:player2.position.y + cos(player2.zRotation) * 3)
+        let p1Direction = CGPoint(x:player1.position.x - sin(player1.zRotation) * tankMoveSpeed,y:player1.position.y + cos(player1.zRotation) * tankMoveSpeed)
+        let p2Direction = CGPoint(x:player2.position.x - sin(player2.zRotation) * tankMoveSpeed,y:player2.position.y + cos(player2.zRotation) * tankMoveSpeed)
         
         let moveTank1 = SKAction.move(to: p1Direction, duration:0)
         let moveTank2 = SKAction.move(to: p2Direction, duration:0)
         
         player1.run(moveTank1)
         player2.run(moveTank2)
+    }
+    
+    func fireProjectile(player: SKSpriteNode) {
+        let projectile = SKSpriteNode(imageNamed: "defaultProjectile")
+        let direction = CGPoint(x:player.position.x - sin(player.zRotation) * 1000,y:player.position.y + cos(player.zRotation) * 1000)
+        projectile.position = player.position
+        
+        addChild(projectile)
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.isDynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.projectile
+        if(projectile_owner == 1){
+            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.player2
+        }
+        if(projectile_owner == 2){
+            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.player1
+        }
+        
+        
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.none
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        
+        let shoot = SKAction.move(to: direction, duration: 2.0)
+        let shootDone = SKAction.removeFromParent()
+        projectile.run(SKAction.sequence([shoot, shootDone]))
+    }
+    func projectileDidCollideWithPlayer1(projectile: SKSpriteNode, player1: SKSpriteNode) {
+        print("Hit")
+        projectile.removeFromParent()
+        player1.removeFromParent()
+    }
+    func projectileDidCollideWithPlayer2(projectile: SKSpriteNode, player2: SKSpriteNode) {
+        print("Hit")
+        projectile.removeFromParent()
+        player2.removeFromParent()
     }
     
     
@@ -131,24 +194,29 @@ class GameScene: SKScene {
             if (player1Left.contains(location)){
                 p1LeftPressed = true
             }
-            // player 1 shoot button pressed
+                // player 1 shoot button pressed
             else if (player1Right.contains(location)){
-                p1RightPressed = true
+                //                p1RightPressed = true
+                projectile_owner = 1
+                fireProjectile(player: player1)
             }
             
             // player 2 turn button pressed
             if (player2Left.contains(location)){
                 p2LeftPressed = true
             }
-            // player 2 shoot button pressed
+                // player 2 shoot button pressed
             else if (player2Right.contains(location)){
-                p2RightPressed = true
+                //                p2RightPressed = true
+                projectile_owner = 2
+                fireProjectile(player: player2)
+                
             }
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        //        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
         for touch in touches {
             let location = touch.location(in:self)
             // if player 1 turn button no longer pressed
@@ -162,71 +230,21 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-        var player = 0;
+        //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
         for touch in touches {
             let location = touch.location(in:self)
             // if player 1 turn button no longer pressed
             if (player1Left.contains(location)){
-                
                 p1LeftPressed = false
             }
             if (player2Left.contains(location)){
-                
                 p2LeftPressed = false
             }
             if (player1Right.contains(location)){
-                player = 1
-                p1LeftPressed = false
             }
             if (player2Right.contains(location)){
-                player = 2
-                p1LeftPressed = false
             }
         }
-        //guard let touch = touches.first else {
-         //   return
-        //}
-        //let touchLocation = touch.location(in: self)
-        
-        // 2 - Set up initial location of projectile
-        let projectile = SKSpriteNode(imageNamed: "projectile")
-        var direc = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
-        if(player == 1){
-           projectile.position = player1.position
-            
-             direc = CGPoint(x:player1.position.x - sin(player1.zRotation) * 3,y:player1.position.y + cos(player1.zRotation) * 3)
-            
-        }
-        if(player == 2){
-            projectile.position = player2.position
-             direc = CGPoint(x:player2.position.x - sin(player2.zRotation) * 3,y:player2.position.y + cos(player2.zRotation) * 3)
-        }
-       
-        
-        
-        // 3 - Determine offset of location to projectile
-        let offset = projectile.position
-        
-        // 4 - Bail out if you are shooting down or backwards
-        if offset.x < 0 { return }
-        
-        // 5 - OK to add now - you've double checked position
-        addChild(projectile)
-        
-        // 6 - Get the direction of where to shoot
-        let direction = offset.normalized()
-        
-        // 7 - Make it shoot far enough to be guaranteed off screen
-        let shootAmount = direction * 1000
-        
-        // 8 - Add the shoot amount to the current position
-        let realDest = direc * 1000
-        
-        // 9 - Create the actions
-        let actionMove = SKAction.move(to: direc, duration: 2.0)
-        let actionMoveDone = SKAction.removeFromParent()
-        projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -247,7 +265,23 @@ class GameScene: SKScene {
         }
         // player 1 shoot
         if (p1RightPressed) {
-//            player1.zRotation = CGFloat(0-tankRotateSpeed)
+            //            player1.zRotation = CGFloat(0-tankRotateSpeed)
         }
+        
+        boundsChecktanks()
     }
+    
+    func debugDrawPlayableArea(){
+        let shape = SKShapeNode()
+        let path = CGMutablePath()
+        path.addRect(playableRect)
+        shape.path = path
+        shape.strokeColor = SKColor.black
+        shape.lineWidth = 4.0
+        addChild(shape)
+        
+    }
+}
+extension GameScene: SKPhysicsContactDelegate {
+    
 }
