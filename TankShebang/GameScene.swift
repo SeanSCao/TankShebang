a071cb311c4c985e6ef18da82a29e55d9a94fc99
SeanSCao <ssc543@nyu.edu>
2019-11-25 20:00:24 -0500

    changed existing functions to use SKSpriteNode arrays

--- TankShebang/GameScene.swift
+++ TankShebang/GameScene.swift
@@ -10,6 +10,8 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    // Initialize the playable space, makes sure to fit all devices
     let playableRect: CGRect
     
     override init(size: CGSize) {
@@ -24,57 +26,71 @@ class GameScene: SKScene {
         fatalError("init(coder:) has not been implemented")
     }
     
-    let player1 = SKSpriteNode(imageNamed: "tank") //player 1 tank
-    let player1Right = SKSpriteNode(imageNamed: "red") //player 1 shoot button
-    let player1Left = SKSpriteNode(imageNamed: "red") //player 1 turn button
-    var p1LeftPressed = false
-    var p1RightPressed = false
-    
-    let player2 = SKSpriteNode(imageNamed: "tank") //player 2 tank
-    let player2Right = SKSpriteNode(imageNamed: "red") //player 2 shoot button
-    let player2Left = SKSpriteNode(imageNamed: "red") //player 2 turn button
-    var p2LeftPressed = false
-    var p2RightPressed = false
+    var numberOfPlayers = 2
+    var players = [SKSpriteNode]()
+    var playerRightButtons = [SKSpriteNode]()
+    var playerLeftButtons = [SKSpriteNode]()
+    var playerLeftPressed = [false, false, false, false]
     
     let tankMoveSpeed = CGFloat(3)
     let tankRotateSpeed = 0.1 //tank turning speed
-    
     
     override func didMove(to view: SKView) {
         
         backgroundColor = SKColor.white
         
-        // player 1 tank and button positioning
-        player1.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
-        player1Left.position = CGPoint(x: player1Left.size.width/2, y: player1Left.size.height/2)
-        player1Right.position = CGPoint(x: size.width - player1Right.size.width/2, y: player1Right.size.height/2)
-        
-        player1Right.zRotation += .pi/2
-        
-        player1Right.alpha = 0.6
-        player1Left.alpha = 0.6
-        
-        addChild(player1)
-        addChild(player1Right)
-        addChild(player1Left)
-        
-        // player 2 tank and button positioning
-        player2.position = CGPoint(x: size.width * 0.5, y: size.height * 0.9)
-        player2Left.position = CGPoint(x: size.width - player2Left.size.width/2, y: size.height - player2Left.size.height/2)
-        player2Right.position = CGPoint(x: player2Right.size.width/2, y: size.height - player2Right.size.height/2)
-        
-        player2.zRotation += .pi
-        player2Left.zRotation += .pi
-        player2Right.zRotation -= .pi/2
-        
-        player2Right.alpha = 0.6
-        player2Left.alpha = 0.6
-        
-        addChild(player2)
-        addChild(player2Right)
-        addChild(player2Left)
+        initPlayers()
         
         debugDrawPlayableArea()
+        
+    }
+    
+    func initPlayers(){
+        
+        for i in 1...numberOfPlayers {
+            let player = SKSpriteNode(imageNamed: "tank") //player tank
+            let playerLeft = SKSpriteNode(imageNamed: "red") //player turn button
+            let playerRight = SKSpriteNode(imageNamed: "red") //player shoot button
+            
+            
+            if (i==1){
+                // player 1 tank and button positioning
+                player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
+                playerLeft.position = CGPoint(x: playerLeft.size.width/2, y: playerLeft.size.height/2)
+                playerRight.position = CGPoint(x: size.width - playerRight.size.width/2, y: playerRight.size.height/2)
+                
+                playerRight.zRotation += .pi/2
+                
+                playerRight.alpha = 0.6
+                playerLeft.alpha = 0.6
+                
+                addChild(player)
+                addChild(playerRight)
+                addChild(playerLeft)
+            }
+            
+            if (i==2){
+                // player 2 tank and button positioning
+                player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.9)
+                playerLeft.position = CGPoint(x: size.width - playerLeft.size.width/2, y: size.height - playerLeft.size.height/2)
+                playerRight.position = CGPoint(x: playerRight.size.width/2, y: size.height - playerRight.size.height/2)
+                
+                player.zRotation += .pi
+                playerLeft.zRotation += .pi
+                playerRight.zRotation -= .pi/2
+                
+                playerRight.alpha = 0.6
+                playerLeft.alpha = 0.6
+                
+                addChild(player)
+                addChild(playerRight)
+                addChild(playerLeft)
+            }
+            
+            players.append(player)
+            playerLeftButtons.append(playerLeft)
+            playerRightButtons.append(playerRight)
+        }
         
     }
     
@@ -82,47 +98,44 @@ class GameScene: SKScene {
         let bottomLeft = CGPoint(x:0, y:playableRect.minY)
         let topRight = CGPoint(x: size.width, y: playableRect.maxY)
         
-        if player1.position.x <= bottomLeft.x {
-            player1.position.x = bottomLeft.x
+        for i in 1...numberOfPlayers {
+            
+            if players[i-1].position.x <= bottomLeft.x {
+                players[i-1].position.x = bottomLeft.x
+            }
+            if players[i-1].position.x >= topRight.x {
+                players[i-1].position.x = topRight.x
+            }
+            if players[i-1].position.y <= bottomLeft.y {
+                players[i-1].position.y = bottomLeft.y
+            }
+            if players[i-1].position.y >= topRight.y {
+                players[i-1].position.y = topRight.y
+            }
         }
-        if player1.position.x >= topRight.x {
-            player1.position.x = topRight.x
-        }
-        if player1.position.y <= bottomLeft.y {
-            player1.position.y = bottomLeft.y
-        }
-        if player1.position.y >= topRight.y {
-            player1.position.y = topRight.y
-        }
-        
-        if player2.position.x <= bottomLeft.x {
-            player2.position.x = bottomLeft.x
-        }
-        if player2.position.x >= topRight.x {
-            player2.position.x = topRight.x
-        }
-        if player2.position.y <= bottomLeft.y {
-            player2.position.y = bottomLeft.y
-        }
-        if player2.position.y >= topRight.y {
-            player2.position.y = topRight.y
-        }
-        
     }
     
     // Moves tank forward in the direction it is facing
     func moveTanksForward() {
-        // some basic trigonometry to calculate direction tanks are moving
-        let p1Direction = CGPoint(x:player1.position.x - sin(player1.zRotation) * tankMoveSpeed,y:player1.position.y + cos(player1.zRotation) * tankMoveSpeed)
-        let p2Direction = CGPoint(x:player2.position.x - sin(player2.zRotation) * tankMoveSpeed,y:player2.position.y + cos(player2.zRotation) * tankMoveSpeed)
         
-        let moveTank1 = SKAction.move(to: p1Direction, duration:0)
-        let moveTank2 = SKAction.move(to: p2Direction, duration:0)
-        
-        player1.run(moveTank1)
-        player2.run(moveTank2)
+        for i in 1...numberOfPlayers {
+            var direction = CGPoint(x: 0, y:0)
+            
+            // some basic trigonometry to calculate direction tanks are moving
+            if (i==1){
+                direction = CGPoint(x:players[0].position.x - sin(players[0].zRotation) * tankMoveSpeed,y:players[0].position.y + cos(players[0].zRotation) * tankMoveSpeed)
+            }
+            
+            if (i==2){
+                direction = CGPoint(x:players[1].position.x - sin(players[1].zRotation) * tankMoveSpeed,y:players[1].position.y + cos(players[1].zRotation) * tankMoveSpeed)
+            }
+            
+            let moveTank = SKAction.move(to: direction, duration:0)
+            players[i-1].run(moveTank)
+        }
     }
     
+    // fire projectile in direction player tank is facing
     func fireProjectile(player: SKSpriteNode) {
         let projectile = SKSpriteNode(imageNamed: "defaultProjectile")
         let direction = CGPoint(x:player.position.x - sin(player.zRotation) * 1000,y:player.position.y + cos(player.zRotation) * 1000)
@@ -151,24 +164,15 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in:self)
-            // player 1 turn button pressed
-            if (player1Left.contains(location)){
-                p1LeftPressed = true
-            }
-                // player 1 shoot button pressed
-            else if (player1Right.contains(location)){
-                //                p1RightPressed = true
-                fireProjectile(player: player1)
-            }
             
-            // player 2 turn button pressed
-            if (player2Left.contains(location)){
-                p2LeftPressed = true
-            }
-                // player 2 shoot button pressed
-            else if (player2Right.contains(location)){
-                //                p2RightPressed = true
-                fireProjectile(player: player2)
+            for i in 1...numberOfPlayers {
+                if (playerLeftButtons[i-1].contains(location)){
+                    playerLeftPressed[i-1] = true
+                }
+                
+                if (playerRightButtons[i-1].contains(location)){
+                    fireProjectile(player: players[i-1])
+                }
             }
         }
     }
@@ -177,12 +181,13 @@ class GameScene: SKScene {
         //        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
         for touch in touches {
             let location = touch.location(in:self)
-            // if player 1 turn button no longer pressed
-            if (player1Left.contains(location)){
-                p1LeftPressed = true
-            }
-            else{
-                p1LeftPressed = false
+            
+            for i in 1...numberOfPlayers {
+                if (playerLeftButtons[i-1].contains(location)){
+                    playerLeftPressed[i-1] = true
+                } else{
+                    playerLeftPressed[i-1] = false
+                }
             }
         }
     }
@@ -191,16 +196,14 @@ class GameScene: SKScene {
         //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
         for touch in touches {
             let location = touch.location(in:self)
-            // if player 1 turn button no longer pressed
-            if (player1Left.contains(location)){
-                p1LeftPressed = false
-            }
-            if (player2Left.contains(location)){
-                p2LeftPressed = false
-            }
-            if (player1Right.contains(location)){
-            }
-            if (player2Right.contains(location)){
+            
+            for i in 1...numberOfPlayers {
+                if (playerLeftButtons[i-1].contains(location)){
+                    playerLeftPressed[i-1] = false
+                }
+                
+                if (playerRightButtons[i-1].contains(location)){
+                }
             }
         }
     }
@@ -214,16 +217,10 @@ class GameScene: SKScene {
         // move all tanks forward
         run(SKAction.run(moveTanksForward))
         
-        // player 1 turn
-        if (p1LeftPressed) {
-            player1.zRotation += CGFloat(tankRotateSpeed)
-        }
-        if (p2LeftPressed) {
-            player2.zRotation += CGFloat(tankRotateSpeed)
-        }
-        // player 1 shoot
-        if (p1RightPressed) {
-            //            player1.zRotation = CGFloat(0-tankRotateSpeed)
+        for i in 1...numberOfPlayers {
+            if (playerLeftPressed[i-1]){
+                players[i-1].zRotation += CGFloat(tankRotateSpeed)
+            }
         }
         
         boundsChecktanks()
