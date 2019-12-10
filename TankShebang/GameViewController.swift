3d48e8fc81b6d06ce379a84b82757e7628c2272b
gaybrogrammer <rbeit508@gmail.com>
2019-12-10 11:47:28 -0500

    Add files via upload

--- TankShebang/GameViewController.swift
+++ TankShebang/GameViewController.swift
@@ -20,6 +20,13 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         let scene = GameScene(size: view.bounds.size)
+        scene.mode = Mode
+        scene.terrain = Terrain
+        scene.kill = Kill
+        scene.sound = sound
+        scene.music = music
+        scene.AutoBalance = AutoBalance
+        scene.Powerups = Powerups
         if(FixedSpawn == "On"){
             scene.startWithShield = true
         }
@@ -49,7 +56,7 @@ class GameViewController: UIViewController {
         if(sound == "On"){
            scene.SFX = true
         }
-        else{
+        if(sound == "Off"){
            scene.SFX = false
         }
         if(Terrain  == "Grass"){
@@ -59,9 +66,10 @@ class GameViewController: UIViewController {
             scene.mapSetting = 2
             
         }
-        scene.viewController = self
         
+        scene.ScatterShot = ScatterShot
         scene.numberOfPlayers = num_players
+        scene.num_players = num_players
         let skView = view as! SKView
         skView.showsFPS = true
         skView.showsNodeCount = true
